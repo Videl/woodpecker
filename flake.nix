4546baa07aac53752a458d9bbef7c2c9eda42b26
Thibaut SMITH <t.smith@planet-work.com>
2022-03-24 22:11:02 +0100

    flake: build with nix from make

--- flake.nix
+++ flake.nix
@@ -28,30 +28,31 @@
           pkgs = nixpkgsFor.${system};
         in
         {
-          woodpecker = pkgs.buildGoModule {
-            pname = "woodpecker";
+          woodpecker-cli = pkgs.stdenv.mkDerivation {
+            name = "woodpecker-cli";
             inherit version;
-            # In 'nix develop', we don't need a copy of the source tree
-            # in the Nix store.
+
+            buildInputs = [
+              pkgs.go
+            ];
+
+            buildPhase = ''
+              export HOME=$PWD
+              make build-cli
+            '';
+
+            installPhase = ''
+              mkdir -p $out/bin
+              mv dist/woodpecker-cli $out/bin/woodpecker-cli
+            '';
+            
             src = ./.;
-
-            # This hash locks the dependencies of this package. It is
-            # necessary because of how Go requires network access to resolve
-            # VCS.  See https://www.tweag.io/blog/2021-03-04-gomod2nix/ for
-            # details. Normally one can build with a fake sha256 and rely on native Go
-            # mechanisms to tell you what the hash should be or determine what
-            # it should be "out-of-band" with other tooling (eg. gomod2nix).
-            # To begin with it is recommended to set this, but one must
-            # remeber to bump this hash when your dependencies change.
-            #vendorSha256 = pkgs.lib.fakeSha256;
-
-            vendorSha256 = "sha256-pQpattmS9VmO3ZIQUFn66az8GSmB4IvYhTTCFn6SUmo=";
           };
         });
 
       # The default package for 'nix build'. This makes sense if the
       # flake provides only one package or there is a clear "main"
       # package.
-      defaultPackage = forAllSystems (system: self.packages.${system}.woodpecker);
+      defaultPackage = forAllSystems (system: self.packages.${system}.woodpecker-cli);
     };
 }
